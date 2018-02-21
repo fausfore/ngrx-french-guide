68918d6b0120576091dcdcf7161bb0496378b9d8
Matias <matiasljubica@gmail.com>
2018-02-21 16:49:00 +0100

    fix

--- ngrx-todos-exemple/src/app/modules/todo-list/components/select-todo/select-todo.component.ts
+++ ngrx-todos-exemple/src/app/modules/todo-list/components/select-todo/select-todo.component.ts
@@ -1,13 +1,13 @@
-import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs/Observable';
-import { tap } from 'rxjs/operators';
+import { TodoListModule } from '@Actions/todo-list.action';
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AppState } from '@StoreConfig';
-import { selectTodoSelected$ } from '@Selectors/todo-list.selector';
-import { TodoListModule } from '@Actions/todo-list.action';
-import { TodoListState, Todo } from '@Models/todo';
 import { Router } from '@angular/router';
+import { Todo } from '@Models/todo';
+import { select, Store } from '@ngrx/store';
+import { selectTodoSelected$ } from '@Selectors/todo-list.selector';
+import { AppState } from '@StoreConfig';
+import { Observable } from 'rxjs/Observable';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-select-todo',
@@ -33,13 +33,12 @@ export class SelectTodoComponent implements OnInit {
   constructor(
     private store: Store<AppState>,
     private router: Router,
-    @Inject(FormBuilder) fb: FormBuilder,
+    @Inject(FormBuilder) fb: FormBuilder
   ) {
   this.selectTodo$ = store
   .pipe(
     select(selectTodoSelected$),
     tap(selectTodos => {
-      console.log('HERE');
       this.selectTodo = selectTodos;
     })
   );
@@ -61,7 +60,6 @@ export class SelectTodoComponent implements OnInit {
   }
 
   updateTodo(formValue) {
-    console.log(formValue);
     const payload = Object.assign(this.selectTodo, formValue);
     this.store.dispatch(new TodoListModule.UpdateTodo(payload));
     return this.router.navigate(['/todo-list/all-todos']);
